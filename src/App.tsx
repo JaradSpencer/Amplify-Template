458ecfcc6938fa2a3479bc3e1542f6057cfdbecf
JaradSpencer <audiospencer@gmail.com>
2026-09-18 13:29:14 -0500

    viteonly

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>Vite Install Only</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -27,7 +27,7 @@ function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 App successfully hosted. Try creating a new todo list.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
